83004ca6239801df4cf5a19e6b4fc6a32c201f62
rameshchenthur <ramesh.chenthur@nbcuni.com>
2014-12-31 18:02:46 +0530

    Update GenreCreation_Action.java
    Latest code updated by rameshchenthur on dec31-2014

--- USHE_Automation/src/appModules/GenreCreation_Action.java
+++ USHE_Automation/src/appModules/GenreCreation_Action.java
@@ -15,7 +15,7 @@ public class GenreCreation_Action {
 	
 	public static void GenreCreation(WebDriver driver) throws Exception{
 		
-		ExcelUtils.setExcelFile(Constant.Path_TestData + Constant.File_TestData,"Genre");
+		ExcelUtils.setExcelFile(Constant.Path_TestData + Constant.File_TestData,"Genre");//Open the Data excel file and focus on to the particular sheet
 		
 		for(int i=1;i<=ExcelUtils.getRowCount();i++)
 		{
@@ -24,8 +24,7 @@ public class GenreCreation_Action {
 			Thread.sleep(20000);
 			
 			if(driver.getTitle().contains("Genre")){
-				
-				driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[title='Genre dialog']")));
+
 				GenreName = ExcelUtils.getCellData(i, 0);
 				Genre_OR.txtbx_GenName(driver).sendKeys(GenreName);
 								
@@ -34,7 +33,6 @@ public class GenreCreation_Action {
 				Genre_OR.txtbx_GenDesc(driver).sendKeys(GenDesc);
 				
 				driver.switchTo().defaultContent();
-				driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[title='Genre dialog']")));
 				
 				Genre_OR.btn_Save(driver).click();
 				Thread.sleep(20000);
@@ -46,67 +44,26 @@ public class GenreCreation_Action {
 				
 				CommonFunctions.scrShot(Constant.screenShots_Path, "Genre");
 				
-				ApplicationLinks.link_CloseWindow(driver).click();
+				ApplicationLinks.link_Menu(driver).click();
 				Thread.sleep(10000);
-				
-				driver.switchTo().defaultContent();
 				
 				ApplicationLinks.link_Structure(driver).click();
 			    Thread.sleep(10000);
-			       
-			    driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[title='Structure dialog']")));
 			    
 			    ApplicationLinks.link_Taxonomy(driver).click();
 			    Thread.sleep(10000);
-			    
-			    driver.switchTo().defaultContent();
-			    
-			    driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[title='Taxonomy dialog']")));
-			    
+		    
 			    ApplicationLinks.link_listofGenre(driver).click();
 			    Thread.sleep(10000);
-			    
-		 	    driver.switchTo().defaultContent();
-		 	    
-  	 	        driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[title='Genre dialog']")));
-	    	    Thread.sleep(10000);
+   	    
+	    	            ApplicationLinks.link_Genre(driver).click();
+	    	            Thread.sleep(10000);
 	    	    
-	    	    ApplicationLinks.link_Genre(driver).click();
-	    	    Thread.sleep(10000);
+	    	            ApplicationLinks.link_Edit(driver).click();
+	    	            Thread.sleep(9000);
 	    	    
-	    	    driver.switchTo().defaultContent();
+	    	             genre_Node=CommonFunctions.extractNodeNumber();
 	    	    
-	    	    ApplicationLinks.link_Edit(driver).click();
-	    	    Thread.sleep(9000);
-	    	    
-	    	    genre_Node=CommonFunctions.extractNumber();
-	    	    
-	    	    driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[title='"+GenreName+" dialog']")));
-	    	    
-	    	    ApplicationLinks.link_CloseWindow(driver).click();
-				Thread.sleep(10000);
-				
-				driver.switchTo().defaultContent();
-				
-			/* verification og genre in title page
-				ApplicationLinks.link_CloseWindow(driver).click();
-				Thread.sleep(10000);
-				
-				Application_Navigations.crtTitle_Navigate(driver);
-				
-				Thread.sleep(30000);
-				
-				driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[title='Create Title dialog']")));
-				
-				if(driver.findElement(By.cssSelector("body")).getText().contains(GenreName)){
-			       
-			        System.out.println(GenreName +"is present in the page");
-			     }
-				
-				ApplicationLinks.link_CloseWindow(driver).click();
-				Thread.sleep(10000);
-				
-				driver.switchTo().defaultContent();*/
 				
 			}
 		}
