d2dff2ff167b37fe11d544a92fa9a359356729c1
rameshchenthur <ramesh.chenthur@nbcuni.com>
2014-12-31 17:24:45 +0530

    Update Constant.java
    Latest code updated by rameshchenthur on dec31-2014

--- USHE_Automation/src/utility/Constant.java
+++ USHE_Automation/src/utility/Constant.java
@@ -4,24 +4,21 @@ package utility;
 
 public class Constant {
 	
-	 public static final String URL = "http://nbcuushe.qa.publisher7.com/";
+	 public static final String URL = "http://use-int.nbcuext.com/user";
 	 
-     public static final String Username = "admin@publisher.nbcuni.com";
-
-     public static final String Password = "pa55word";
-
-     public static final String Path_TestData = "C://Users//ra821463//Documents//Userdata//WIP//Auto_USHE//NBCU_Automation//src//testData//";
+     public static final String Path_TestData = "C:\\Users\\ra821463\\Documents\\Userdata\\WIP\\Auto_USHE\\USHE_Auto\\src\\testData\\";
 
      public static final String File_TestData = "TestData.xlsx";
            
      public static final String File_Result = "Result.xlsx";
      
-     public static final String Path_Logfile="C://Users//ra821463//Desktop//log4j.properties";
+     public static final String Path_Logfile="C:\\Users\\ra821463\\Documents\\Userdata\\WIP\\Auto_USHE\\USHE_Auto\\log4j.properties";
      
-     public static final String screenShots_Path="C://Users//ra821463//Documents//Userdata//WIP//Auto_USHE//NBCU_Automation//ScreenShots//";
+     public static final String screenShots_Path="C:\\Users\\ra821463\\Documents\\Userdata\\WIP\\Auto_USHE\\USHE_Auto\\ScreenShots\\";
      
-     public static final String images_Path="C:\\Users\\ra821463\\Documents\\Userdata\\WIP\\Auto_USHE\\NBCU_Automation\\Images\\";
+     public static final String images_Path="C:\\Users\\ra821463\\Documents\\Userdata\\WIP\\Auto_USHE\\USHE_Auto\\Images\\";
      
-     public static final String imageUpcode_Path="C:\\Users\\ra821463\\Documents\\Userdata\\WIP\\Auto_USHE\\NBCU_Automation\\upload.exe";
-
+     public static final String imageUpcode_Path="C:\\Users\\ra821463\\Documents\\Userdata\\WIP\\Auto_USHE\\USHE_Auto\\upload.exe";
+     
 }
+
