c6c940835f9d9fcc949c1a71edd1bc1435fa315f
rameshchenthur <ramesh.chenthur@nbcuni.com>
2014-12-31 18:17:12 +0530

    Update RetailerCreation_Action.java
    Latest code updated by rameshchenthur on dec31-2014

--- USHE_Automation/src/appModules/RetailerCreation_Action.java
+++ USHE_Automation/src/appModules/RetailerCreation_Action.java
@@ -1,6 +1,5 @@
 package appModules;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import utility.Log;
@@ -15,46 +14,42 @@ public class RetailerCreation_Action {
 	
   public static void RetailerCreate(WebDriver driver) throws Exception{
 	
-	   ExcelUtils.setExcelFile(Constant.Path_TestData + Constant.File_TestData,"Retailer");
+	ExcelUtils.setExcelFile(Constant.Path_TestData + Constant.File_TestData,"Retailer");
 			
-		for(int i=1;i<=ExcelUtils.getRowCount();i++)
-		{
+	for(int i=1;i<=ExcelUtils.getRowCount();i++)
+	{
 			 
-	    Application_Navigations.crtRetailer_Navigate(driver);
-	    Thread.sleep(10000);
+	     Application_Navigations.crtRetailer_Navigate(driver);
+	     Thread.sleep(10000);
 		
-	    if(driver.getTitle().contains("Create Retailer")){
-	    	
-	      driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[title='Create Retailer dialog']")));
+	     if(driver.getTitle().contains("Create Retailer"))
+	     {
+	    	 retailerTitle = ExcelUtils.getCellData(i, 0);
+	    	 Retailer_OR.txtbx_Title(driver).sendKeys(retailerTitle);
 		
-		  retailerTitle = ExcelUtils.getCellData(i, 0);
-		  Retailer_OR.txtbx_Title(driver).sendKeys(retailerTitle);
-		
-		  String FormatOption = ExcelUtils.getCellData(i, 1);
-		  Select foption = new Select(Retailer_OR.list_FormatOption(driver)); 
-		  foption.selectByVisibleText(FormatOption);
+	    	 String FormatOption = ExcelUtils.getCellData(i, 1);
+	    	 Select foption = new Select(Retailer_OR.list_FormatOption(driver)); 
+	    	 foption.selectByVisibleText(FormatOption);
         
-		  String searUrl = ExcelUtils.getCellData(i, 2);
-		  Retailer_OR.txtbx_SearUrl(driver).sendKeys(searUrl);
+	    	 String searUrl = ExcelUtils.getCellData(i, 2);
+	    	 Retailer_OR.txtbx_SearUrl(driver).sendKeys(searUrl);
 				
-		  Retailer_OR.link_PubOption(driver).click();
+	    	 Retailer_OR.link_PubOption(driver).click();
 				 
-		  String ModState = ExcelUtils.getCellData(i, 3);
-		  Select pstate = new Select(Retailer_OR.list_ModState(driver)); 
-		  pstate.selectByVisibleText(ModState);
+	    	 String ModState = ExcelUtils.getCellData(i, 3);
+	    	 Select pstate = new Select(Retailer_OR.list_ModState(driver)); 
+	    	 pstate.selectByVisibleText(ModState);
         	
-		  String assignTo = ExcelUtils.getCellData(i, 4);
-		  Retailer_OR.txtbx_AssignTo(driver).clear();
-		  Retailer_OR.txtbx_AssignTo(driver).sendKeys(assignTo);
+	    	 String assignTo = ExcelUtils.getCellData(i, 4);
+	    	 Retailer_OR.txtbx_AssignTo(driver).clear();
+	    	 Retailer_OR.txtbx_AssignTo(driver).sendKeys(assignTo);
 		
-		  String logMsg = ExcelUtils.getCellData(i, 5);
-		  Retailer_OR.txtbx_Logmess(driver).sendKeys(logMsg);
+	    	 String logMsg = ExcelUtils.getCellData(i, 5);
+	    	 Retailer_OR.txtbx_Logmess(driver).sendKeys(logMsg);
 		
-		  Retailer_OR.btn_Save(driver).click();
+	    	 Retailer_OR.btn_Save(driver).click();
 		
-		  Thread.sleep(10000);
-		
-		  driver.switchTo().defaultContent();
+	    	 Thread.sleep(10000);
 		
 		  if(driver.getTitle().contains(retailerTitle))
 		  	{
