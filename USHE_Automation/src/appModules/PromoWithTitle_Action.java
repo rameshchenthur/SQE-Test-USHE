f555ff6fb8c1d55259eef13718cf75a2806710f6
rameshchenthur <ramesh.chenthur@nbcuni.com>
2014-12-31 18:12:44 +0530

    Update PromoWithTitle_Action.java
    Latest code updated by rameshchenthur on dec31-2014

--- USHE_Automation/src/appModules/PromoWithTitle_Action.java
+++ USHE_Automation/src/appModules/PromoWithTitle_Action.java
@@ -25,42 +25,31 @@ public static String promotitle;
 		
 		   Thread.sleep(20000);
 		   if(driver.getTitle().contains("Create Promo"))
-            {
-		
-		       driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[title='Create Promo dialog']")));
-
+                    {
 		       Promo_OR.btn_existingTitle(driver).click();
 		       Thread.sleep(5000);
-	       
-      
-		       //code start
 		       
-		       String owels[]={"a","e","i","o","u"};
-				
-				for(String element : owels) 
-				{
-				  Promo_OR.txtbx_existingTitle(driver).clear();
-				  Promo_OR.txtbx_existingTitle(driver).sendKeys(element);
-				  Thread.sleep(5000);
-				  int Size=driver.findElements(By.xpath("//div[@id='autocomplete']/ul/li/div/div[contains(text(),element)]")).size();
-				  for(int j=1;j<Size;j++)
+		       String promo_Title=ExcelUtils.getCellData(i, 0);
+		       Promo_OR.txtbx_existingTitle(driver).sendKeys(promo_Title);
+		       
+		       int Size=driver.findElements(By.xpath("//div[@id='autocomplete']/ul/li/div/div[contains(text(),'"+promo_Title+"')]")).size();
+				  for(int j=1;j<=Size;j++)
 		          {  
 		    	      WebElement titleDropDown=driver.findElement(By.xpath("//div[@id='autocomplete']/ul/li["+j+"]/div/div"));
-		    	         if(titleDropDown.getText().contains(element))
+		    	      if(titleDropDown.getText().contains(promo_Title))
 		    	         {
-		    	        	 titleDropDown.click();
+		    	    	      titleDropDown.click();
 		    		          break;
 		    	         }
 		    	  }
-				   break;
-				}
-				Promo_OR.btn_addTitle(driver).click();
-				Thread.sleep(4000);
+		       
+			Promo_OR.btn_addTitle(driver).click();
+			Thread.sleep(4000);
 				
-			    promotitle=ExcelUtils.getCellData(i, 1);
-			    Promo_OR.txtbx_pTitle(driver).sendKeys(promotitle);
+		       promotitle=ExcelUtils.getCellData(i, 1);
+		       Promo_OR.txtbx_pTitle(driver).sendKeys(promotitle);
 	
-				//Image upload starts
+		       //Image upload starts
 		
 		       Promo_OR.btn_Image(driver).click();
 		       Thread.sleep(9000);
@@ -89,18 +78,14 @@ public static String promotitle;
 		       Thread.sleep(20000);
                                     
 		       driver.switchTo().defaultContent();
-		       
-		       driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[title='Create Promo dialog']")));
-                 
+		       		                      
 		       driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[title='Rich Text AreaPress ALT-F10 for toolbar. Press ALT-0 for help']")));
         
 		       String titleBody = ExcelUtils.getCellData(i, 3);
 		       Promo_OR.txtbx_Descrption(driver).sendKeys(titleBody);
         
 		       driver.switchTo().defaultContent();
-        
-		       driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[title='Create Promo dialog']")));
-     
+             
 		       String ModState = ExcelUtils.getCellData(i, 4);
 		       Select mstate = new Select(Promo_OR.list_ModState(driver)); 
 		       mstate.selectByVisibleText(ModState);
@@ -118,13 +103,11 @@ public static String promotitle;
 		       if(driver.getTitle().contains(promotitle))
 		       	{
 		    	   Log.info("Promo "+promotitle+" has been created.");
-    	       	}
+    	       	        }
 		       CommonFunctions.scrShot(Constant.screenShots_Path, "PrmowithTitle");
-            }
-		   else
-		   {
+                   }else{
 			   Log.info("Time Out");
-		   }
+		        }
 		}
 		
 	}
