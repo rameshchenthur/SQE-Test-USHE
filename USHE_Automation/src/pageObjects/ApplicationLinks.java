e48ad73de2388abb6fd9cac4d71f7b4bc3ccb946
rameshchenthur <ramesh.chenthur@nbcuni.com>
2014-12-31 17:26:51 +0530

    Update ApplicationLinks.java
    Latest code updated by rameshchenthur on dec31-2014

--- USHE_Automation/src/pageObjects/ApplicationLinks.java
+++ USHE_Automation/src/pageObjects/ApplicationLinks.java
@@ -10,8 +10,19 @@ import utility.Log;
 
 public class ApplicationLinks {
 	
-static WebElement element = null;
+    static WebElement element = null;
 
+    public static WebElement link_Menu(WebDriver driver) {
+   	 
+        element = driver.findElement(By.linkText("Menu"));
+
+        Log.info("Menu link found");
+
+         return element;
+
+         }
+  
+    
     public static WebElement link_Home(WebDriver driver) {
 	 
        element = driver.findElement(By.xpath(".//*[@id='admin-menu-icon']/li/a/span"));
@@ -70,7 +81,7 @@ static WebElement element = null;
 
          return element; 
 
-}
+         }
 	public static WebElement link_AddContent(WebDriver driver) {
 
 		element = driver.findElement(By.linkText("Add content"));
@@ -96,7 +107,7 @@ static WebElement element = null;
 		 
         element = driver.findElement(By.linkText("Retailer"));
 
-        Log.info("Promo link found");
+        Log.info("Retailer link found");
 
         return element;
 
@@ -176,75 +187,75 @@ static WebElement element = null;
 	
 	public static WebElement link_AddTermForGenre(WebDriver driver){
 		
-		element=driver.findElement(By.xpath(".//*[@id='edit-6-add']"));
+		element=driver.findElement(By.xpath(".//*[@id='edit-2-add']"));
 		
 		Log.info("Addterm link found");
 		
 		return element;
-	}
+	    }
 	
 	
 	
-  public static WebElement link_listofGenre(WebDriver driver){
+    public static WebElement link_listofGenre(WebDriver driver){
 		
-		element=driver.findElement(By.xpath(".//*[@id='edit-6-list']"));
+		element=driver.findElement(By.xpath(".//*[@id='edit-2-list']"));
 		
 		Log.info("listoff genere found");
 		
 		return element;
-	  }
+    	}
   
   
-  public static WebElement link_ClickParticularGenre(WebDriver driver){
+    public static WebElement link_ClickParticularGenre(WebDriver driver){
 		
 		element=driver.findElement(By.linkText(GenreCreation_Action.GenreName));
 		
 		Log.info("genere found");
 		
 		return element;
-	  }
+    	}
   
   
-  public static WebElement link_Edit(WebDriver driver){
+    public static WebElement link_Edit(WebDriver driver){
 		
 		element=driver.findElement(By.linkText("Edit"));
 		
 		Log.info("edit link found");
 		
 		return element;
-	  }
+    	}
   
-  public static WebElement list_Month(WebDriver driver){
+    public static WebElement list_Month(WebDriver driver){
 		
 		element=driver.findElement(By.xpath(".//*[@id='ui-datepicker-div']/div/div/select[1]"));
 		
 		Log.info("Month listbox found");
 		
 		return element;
-	  }
+    	}
   
   
-  public static WebElement list_Year(WebDriver driver){
+    public static WebElement list_Year(WebDriver driver){
 		
 		element=driver.findElement(By.xpath(".//*[@id='ui-datepicker-div']/div/div/select[2]"));
  		
 		Log.info("Year listbox found");
 		
 		return element;
-	  }
+    	}
   
   
-  public static WebElement link_Genre(WebDriver driver){
+    public static WebElement link_Genre(WebDriver driver){
 		
 		element=driver.findElement(By.linkText(GenreCreation_Action.GenreName));
 		
 		Log.info("GenreName link found");
 		
 		return element;
-	  }
+    	}
   
   
-  public static WebElement link_Addfile(WebDriver driver) {
+    public static WebElement link_Addfile(WebDriver driver) {
 		 
       element = driver.findElement(By.id("edit-upload_browse"));
 		
@@ -254,7 +265,7 @@ static WebElement element = null;
         
         }
 
-  public static WebElement link_StartUpload(WebDriver driver) {
+    public static WebElement link_StartUpload(WebDriver driver) {
 	 
       element = driver.findElement(By.xpath(".//*[@id='edit-upload_container']/div/div[2]/div[2]/div[1]/div/a[2]"));
 		
@@ -265,7 +276,7 @@ static WebElement element = null;
         }
 
 
-  public static WebElement link_addSocial(WebDriver driver) {
+    public static WebElement link_addSocial(WebDriver driver) {
 	 
        element = driver.findElement(By.xpath(".//*[@id='block-system-main']/div/table[2]/tbody/tr[6]/td[4]/a"));
 		
@@ -273,8 +284,8 @@ static WebElement element = null;
 
        return element;
       
-      }
-  public static WebElement link_file(WebDriver driver) {
+    	}
+    public static WebElement link_file(WebDriver driver) {
 		 
       element = driver.findElement(By.xpath(".//*[@id='overlay-tabs']/li[3]/a"));
 		
@@ -282,9 +293,9 @@ static WebElement element = null;
 
       return element;
      
-     }
+    	}
   
-  public static WebElement link_mpxMedia(WebDriver driver) {
+    public static WebElement link_mpxMedia(WebDriver driver) {
 		 
       element = driver.findElement(By.xpath(".//*[@id='page']/div[1]/ul/li[4]/a"));
 		
@@ -292,9 +303,9 @@ static WebElement element = null;
 
       return element;
      
-     }
+    	}
   
-  public static WebElement link_mpxMediaTitle(WebDriver driver) {
+    public static WebElement link_mpxMediaTitle(WebDriver driver) {
      
 	  element = driver.findElement(By.xpath(".//*[@id='block-system-main']/div/div/div[3]/table/tbody/tr[1]/td[3]/a"));
 	   
@@ -302,9 +313,9 @@ static WebElement element = null;
 
       return element;
      
-     }
+     	}
   
-  public static WebElement edit_favTitle(WebDriver driver) {
+    public static WebElement edit_favTitle(WebDriver driver) {
 	     
 	  element = driver.findElement(By.xpath("//*[@id='ctools-button-2']/div[2]/ul/li[1]/a"));
 	   
@@ -312,16 +323,25 @@ static WebElement element = null;
 
       return element;
      
-     }
+    	}
   
-  public static WebElement edit_recVideo(WebDriver driver) {
+    public static WebElement edit_recVideo(WebDriver driver) {
 	     
 	  element = driver.findElement(By.xpath("//*[@id='ctools-button-1']/div[2]/ul/li[1]/a"));
 	   
-	   Log.info("edit for rec video link found");
+	  Log.info("edit for rec video link found");
 
       return element;
      
-     }
- 
+      }
+    
+    public static WebElement link_Queues(WebDriver driver) {
+	     
+  	  element = driver.findElement(By.linkText("Queues"));
+  	   
+  	  Log.info("Queues link found");
+
+        return element;
+       
+        }
 }
